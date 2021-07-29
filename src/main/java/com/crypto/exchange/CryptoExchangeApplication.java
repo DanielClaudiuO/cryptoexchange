1fb0e98404828459c9b8b11fb1e9f3d25b809d4c
David Pavix <davidpvx@gmail.com>
2021-07-29 13:03:44 +0300

    COE-1 database setup first commit

--- src/main/java/com/crypto/exchange/CryptoExchangeApplication.java
+++ src/main/java/com/crypto/exchange/CryptoExchangeApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CryptoExchangeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CryptoExchangeApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoExchangeApplication.class, args);
+    }
 }
