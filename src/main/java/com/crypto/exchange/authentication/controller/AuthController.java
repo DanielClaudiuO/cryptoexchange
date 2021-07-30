b054efd3eee0f9d7936a0ca6abd9e308377abdf0
Daniel <daniel.oanta@gmail.com>
2021-07-30 14:15:11 +0300

    COE-3- added register controller

--- src/main/java/com/crypto/exchange/authentication/controller/AuthController.java
+++ src/main/java/com/crypto/exchange/authentication/controller/AuthController.java
@@ -1,5 +1,29 @@
 package com.crypto.exchange.authentication.controller;
 
+import com.crypto.exchange.authentication.biz.repository.UserRepository;
+import com.crypto.exchange.authentication.biz.service.RegisterService;
+import com.crypto.exchange.authentication.model.dto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/users")
 public class AuthController {
+
+    @Autowired
+    RegisterService registerService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity<UserDto> registerUser(@RequestBody UserDto user) {
+        return new ResponseEntity<>(registerService.registerUser(user), HttpStatus.CREATED);
+    }
 
 }
