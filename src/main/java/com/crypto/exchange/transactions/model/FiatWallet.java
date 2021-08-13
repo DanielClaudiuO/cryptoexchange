864e9f110a35b944bec2f7d0308a10b87ef82368
Daniel <daniel.oanta@gmail.com>
2021-08-13 11:50:14 +0300

    COE-7- added wallet model prototype

---
+++ src/main/java/com/crypto/exchange/transactions/model/FiatWallet.java
@@ -0,0 +1,34 @@
+package com.crypto.exchange.transactions.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Accessors(chain = true)
+@Table(name = "fiat_wallet")
+
+public class FiatWallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "balance")
+    private double balance;
+}
---
+++ src/main/java/com/crypto/exchange/transactions/model/Wallet.java
@@ -0,0 +1,41 @@
+package com.crypto.exchange.transactions.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Accessors(chain = true)
+@Table(name = "wallet")
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "walletId")
+    private List<CryptoWallet> cryptoWallet;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "walletId")
+    private List<FiatWallet> fiatWallets;
+
+}
